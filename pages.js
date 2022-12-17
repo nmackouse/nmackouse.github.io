c0120a8432fff4f1dfe7f5221f48caf8670f048a
nmackouse <nmackouse@gmail.com>
2022-12-17 15:21:31 -0500

    Revert "test"
    This reverts commit 78c00c49ce0efb16c950c4ebc0f1923b9867d161.

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/nmackouse/nmackouse.github.io.git";
+const repoURL = "https://github.com/nmackouse/nmackouse.github.io";
 
 ghpages.publish(
   pathname,
